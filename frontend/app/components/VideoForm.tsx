055276f19b1df2a2c06d7e75ad60fc56e0188291
TimHi <TimHiller@live.de>
2025-12-24 16:53:03 +0100

    adding tooltip

--- frontend/app/components/VideoForm.tsx
+++ frontend/app/components/VideoForm.tsx
@@ -1,17 +1,30 @@
-import TextField from "@mui/material/TextField";
-
+import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
 
 export interface VideoProps {
-    setURL: (value: string) => void,
-    setMusicBrainzId: (value: string) => void
+	setURL: (value: string) => void;
+	setMusicBrainzId: (value: string) => void;
 }
 
 export default function VideoForm(props: VideoProps) {
-    
-    return (
-        <div className="video-form">
-            <TextField id="outlined-basic" onChange={(v) => props.setURL(v.target.value)} variant="outlined" placeholder="youtube.com/bla" label="Video URL" />
-            <TextField id="outlined-basic" onChange={(v) => props.setMusicBrainzId(v.target.value)} variant="outlined" placeholder="e.g. 12a5b094-3804-4c97-82b8-9c7cc5d4f4ab" label="Musicbrainz Track ID" />
-        </div>
-    );
+	return (
+		<div className='video-form'>
+			<TextField
+				id='outlined-basic'
+				onChange={(v) => props.setURL(v.target.value)}
+				variant='outlined'
+				placeholder='youtube.com/bla'
+				label='Video URL'
+			/>
+			<Tooltip title='The MusicBrainz Recording ID helps to tag the downloaded audio file with correct metadata. Important: recording ID, not release ID!'>
+				<TextField
+					id='outlined-basic'
+					onChange={(v) => props.setMusicBrainzId(v.target.value)}
+					variant='outlined'
+					placeholder='e.g. 12a5b094-3804-4c97-82b8-9c7cc5d4f4ab'
+					label='Musicbrainz Recording ID'
+				/>
+			</Tooltip>
+		</div>
+	);
 }
